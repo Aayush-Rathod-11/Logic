96a7039b401eb429ae1a6a860ec3599ef86c0ebc
Aayush Rathod <ayushrathod2411@gmail.com>
2026-07-25 10:46:35 +0530

    Time: 442 ms (94.85%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,9 +1,5 @@
--- Write your PostgreSQL query statement below
-SELECT
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
+/* Write your PL/SQL query statement below */
+SELECT p.firstname, p.lastname, a.city, a.state
 FROM Person p
 LEFT JOIN Address a
-    ON p.personId = a.personId;
+ON p.personId = a.personId;
