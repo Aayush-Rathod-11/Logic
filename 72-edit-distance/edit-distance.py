2724dd7bbed164ad06ff14e65eab35cc7368416a
Aayush Rathod <ayushrathod2411@gmail.com>
2026-08-02 08:56:52 +0530

    Time: 71 ms (88.60%) | Memory: 12.4 MB (93.07%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        if len(word1) < len(word2):
+            word1, word2 = word2, word1
+
+        m, n = len(word1), len(word2)
+
+        dp = list(range(n + 1))
+
+        for i in range(1, m + 1):
+            prev = dp[0]
+            dp[0] = i
+
+            for j in range(1, n + 1):
+                temp = dp[j]
+
+                if word1[i - 1] == word2[j - 1]:
+                    dp[j] = prev
+                else:
+                    dp[j] = 1 + min(
+                        dp[j],      # delete
+                        dp[j - 1],  # insert
+                        prev        # replace
+                    )
+
+                prev = temp
+
+        return dp[n]
