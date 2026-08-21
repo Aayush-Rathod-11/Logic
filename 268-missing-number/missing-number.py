6f556c6355e34f74cfae3f533eacf7007824278a
Aayush Rathod <ayushrathod2411@gmail.com>
2026-08-21 08:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (40.77%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         result = n
 
