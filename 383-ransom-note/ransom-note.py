d31411d5caf4f9b1378945db358c50275576971d
Aayush Rathod <ayushrathod2411@gmail.com>
2026-08-28 09:25:44 +0530

    Time: 16 ms (67.75%) | Memory: 19.6 MB (33.67%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+
+        count = [0] * 26
+
+        for c in magazine:
+            count[ord(c) - 97] += 1
+
+        for c in ransomNote:
+            i = ord(c) - 97
+            count[i] -= 1
+
+            if count[i] < 0:
+                return False
+
+        return True
